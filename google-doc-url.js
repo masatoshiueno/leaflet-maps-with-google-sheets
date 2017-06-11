d918bc2e9e985a85b620ed09b539c7cdb76a64fb
masatoshiueno <masatoshi.ueno@gmail.com>
2017-06-11 16:58:49 -0700

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1odBrbfWyYSJ1zo1Bn4L34REprQkQX7y49pYaKIj5o9o/edit#gid=0';
